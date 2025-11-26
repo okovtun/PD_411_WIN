17aa1369a19fc6c41ec8fa188e9d7e99f012265e
User <User@DESKTOP-QHG18FL>
2025-11-26 16:09:47 +0300

    Placeholder DONE

---
+++ WPF/CustomTextboxControl/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomTextboxControl.View.UserControls
+{
+	/// <summary>
+	/// Interaction logic for ClearableTextBox.xaml
+	/// </summary>
+	public partial class ClearableTextBox : UserControl
+	{
+
+		private string placeholder;
+		public string Placeholder
+		{
+			get { return placeholder; }
+			set { placeholder = tbPlaceholder.Text = value; }
+		}
+
+
+		public ClearableTextBox()
+		{
+			InitializeComponent();
+		}
+
+		private void TxtInput_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			tbPlaceholder.Visibility = txtInput.Text == "" ? Visibility.Visible : Visibility.Collapsed;
+			//if (txtInput.Text.Length > 16) Window.GetWindow(this).Focus();
+			//if (txtInput.Text.Length > 16) txtInput.Text = txtInput.Text.Substring(0, 16);
+		}
+
+		private void BtnClear_Click(object sender, RoutedEventArgs e)
+		{
+			txtInput.Text = "";
+		}
+	}
+}
